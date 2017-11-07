0f9cb67e81cf210696b261bd2c16c8d463112c60
danpowers <danpowers@users.noreply.github.com>
2017-11-07 17:51:14 -0600

    Update endpoint_scan.py

--- endpoint_scan.py
+++ endpoint_scan.py
@@ -26,7 +26,7 @@ def scan(InputFile):
             endpoint, uuid, hostname, port, _ = entry
 
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(1)
+            s.settimeout(5)
 
             try:
                 s.connect((hostname, int(port)))
